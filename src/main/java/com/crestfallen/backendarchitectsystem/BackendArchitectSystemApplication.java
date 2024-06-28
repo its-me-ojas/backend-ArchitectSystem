a6f1f50b12288d04429a03957c5bffb70e030adb
its-me-ojas <crestfallenwizard@gmail.com>
2024-06-29 00:32:52 +0530

    init files and config

---
+++ src/main/java/com/crestfallen/backendarchitectsystem/BackendArchitectSystemApplication.java
@@ -0,0 +1,13 @@
+package com.crestfallen.backendarchitectsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendArchitectSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendArchitectSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/crestfallen/backendarchitectsystem/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.crestfallen.backendarchitectsystem.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return new ResponseEntity<>("hello", HttpStatus.OK);
+    }
+}

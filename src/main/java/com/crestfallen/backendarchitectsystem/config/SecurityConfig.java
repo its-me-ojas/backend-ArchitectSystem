954ca190d0a13b1b56298897498dec68ef512645
its-me-ojas <crestfallenwizard@gmail.com>
2024-06-29 11:16:20 +0530

    adding login authentication

--- src/main/java/com/crestfallen/backendarchitectsystem/config/SecurityConfig.java
+++ src/main/java/com/crestfallen/backendarchitectsystem/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.crestfallen.backendarchitectsystem.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -33,7 +35,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(customizer -> customizer.disable())
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/register", "login")
+                        .requestMatchers("/register", "/login")
                         .permitAll()
                         .anyRequest().authenticated())
 //                .formLogin(Customizer.withDefaults())
@@ -42,5 +44,10 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
--- src/main/java/com/crestfallen/backendarchitectsystem/controller/PlayerController.java
+++ src/main/java/com/crestfallen/backendarchitectsystem/controller/PlayerController.java
@@ -3,6 +3,9 @@ package com.crestfallen.backendarchitectsystem.controller;
 import com.crestfallen.backendarchitectsystem.model.Player;
 import com.crestfallen.backendarchitectsystem.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +15,24 @@ public class PlayerController {
 
     @Autowired
     private PlayerService playerService;
+    @Autowired
+    AuthenticationManager authenticationManager;
 
     @PostMapping("register")
     public Player register(@RequestBody Player player) {
         return playerService.saveUser(player);
+    }
+
+    @PostMapping("login")
+    public String login(@RequestBody Player player) {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(
+                        player.getUsername(),
+                        player.getPassword()
+                ));
+        if (authentication.isAuthenticated())
+            return "Success";
+        else
+            return "Failed";
     }
 }
